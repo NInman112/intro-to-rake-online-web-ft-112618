b0ee1b73c339de9cf8f213020f537a2ddaa3d9df
Nick Inman <nick@theoatmeal.com>
2019-01-12 21:18:55 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
 require 'sqlite3'
 require 'pry'
-
+require 'bundler/setup'
+Bundler.require
 require_relative "../lib/student.rb"
 
-DB = {:conn => SQLite3::Database.new("db/students.db")}
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artist.sqlite"
+)
